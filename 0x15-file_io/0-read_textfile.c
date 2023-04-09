c179069db5a3e7566c8434cba1dcb1ac0f98a2d5
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-04-08 20:48:12 -0700

    read a text file and prints to STDOUT

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,19 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buff;
-	ssize_t fl;
+	ssize_t fd;
 	ssize_t x;
 	ssize_t y;
 
-	fl = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-	if (fl == -1)
+	if (fd == -1)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
-	y = read(fl, buff, letters);
+	y = read(fd, buff, letters);
 	x = write(STDOUT_FILENO, buff, y);
 
 	free(buff);
-	close(fl);
+	close(fd);
 	return (x);
 }
