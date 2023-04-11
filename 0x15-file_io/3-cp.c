3093ca03f741ee7af035c16a04e8c3dcdc97a408
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-04-10 19:03:50 -0700

    copies a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	out_fd = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	while ((b_read = read(in_fd, buffer, 1024)) > 0)
 	{
-		if (out_fd == -1 || (write(out_fd, buffer, b_read) != b_read))
+		if (out_fd == -1 || (write(out_fd, buffer, b_read == -1)))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	}
 	if (b_read < 0)
 	{
-		dprintf(STDERR_FILENO, "Erro: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	close_in = close(in_fd);
