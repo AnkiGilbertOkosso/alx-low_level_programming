aeea774eac805ad11c79f52c4f15bc1d037e24b9
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2023-12-13 09:07:54 -0800

    task 102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -21,8 +21,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
-		position = low + (((double)(high - low) / (array[high] -
-						array[low])) * (value - array[low]));
+		position = low + (((double)(high - low) / (array[high] - array[
+						low])) * (value - array[low]));
 
 		printf("Value checked array[%lu] = [%d]\n", position, array[position]);
 
